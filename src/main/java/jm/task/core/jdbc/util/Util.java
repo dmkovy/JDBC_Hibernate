710fcb05cbaa07705a53dfb9f01e5a16de81827d
Dmitry Kovalchuk <andycoutur@gmail.com>
2022-11-29 14:45:52 +0200

    First commit. Тесты пройдены

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,24 @@
 package jm.task.core.jdbc.util;
 
-public class Util {
-    // реализуйте настройку соеденения с БД
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public final class Util {
+    private static final String URL = "jdbc:mysql://localhost:3306/users_db";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "root";
+
+    private Util() {
+    }
+
+    public static Connection open() {
+        try {
+            return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
 }
